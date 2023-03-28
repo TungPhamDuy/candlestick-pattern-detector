f41d360a92c299ba4cd4f359149d0b3adc586f28
Tùng Phạm <129039299+TungPhamDuy@users.noreply.github.com>
2023-03-28 19:52:09 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import os, csv
+import talib
+import yfinance as yf
+import pandas
+from datetime import date
+from flask import Flask, escape, request, render_template
+from patterns import candlestick_patterns
+
+app = Flask(__name__)
+
+@app.route('/snapshot')
+def snapshot():
+    with open('datasets/symbols.csv') as f:
+        for line in f:
+            if "," not in line:
+                continue
+            symbol = line.split(",")[0]
+            data = yf.download(symbol, start="2022-01-01", end=date.today())
+            data.to_csv('datasets/daily/{}.csv'.format(symbol))
+
+    return "UPDATE COMPLETED"
+
+@app.route('/')
+def index():
+    pattern  = request.args.get('pattern', False)
+    stocks = {}
+
+    with open('datasets/symbols.csv') as f:
+        for row in csv.reader(f):
+            stocks[row[0]] = {'company': row[1]}
+
+    if pattern:
+        for filename in os.listdir('datasets/daily'):
+            df = pandas.read_csv('datasets/daily/{}'.format(filename))
+            pattern_function = getattr(talib, pattern)
+            symbol = filename.split('.')[0]
+
+            try:
+                results = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
+                last = results.tail(1).values[0]
+
+                if last > 0:
+                    stocks[symbol][pattern] = 'bullish'
+                elif last < 0:
+                    stocks[symbol][pattern] = 'bearish'
+                else:
+                    stocks[symbol][pattern] = None
+            except Exception as e:
+                print('failed on filename: ', filename)
+
+    return render_template('index.html', candlestick_patterns=candlestick_patterns, stocks=stocks, pattern=pattern)
+
+#set FLASK_ENV=development
+#$env:FLASK_DEBUG = "1"
